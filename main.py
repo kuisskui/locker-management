9e8f91ff4bfcc5dc08b3b70a749d35e116d125a2
Siravich Termvadsayanon <93331101+koonwill@users.noreply.github.com>
2023-02-07 13:23:23 +0700

    Add database

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ from fastapi import FastAPI, Body
 import uvicorn
 from typing import Union, Optional, List
 from pydantic import BaseModel
+from pymongo import MongoClient
 from datetime import datetime
+import os
+import urllib
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+DATABASE_NAME = "exceed01"
+COLLECTION_NAME = "locket-management"
+username = os.getenv("user")
+password = urllib.parse.quote(os.getenv('password'))
+MONGO_DB_URL = f"mongodb://{username}:{password}@mongo.exceed19.online"
+MONGO_DB_PORT = 8443
 
 
 class Locker(BaseModel):
@@ -13,12 +26,19 @@ class Locker(BaseModel):
     end: datetime
 
 
+client = MongoClient(f"{MONGO_DB_URL}:{MONGO_DB_PORT}/?authMechanism=DEFAULT")
+
+db = client[DATABASE_NAME]
+
+collection = db[COLLECTION_NAME]
+
 app = FastAPI()
 
 
 @app.get("/")
 def root():
     return {"msg": "pick a locker"}
+
 
 @app.get("/{locker_id}/")
 def locker():
@@ -27,12 +47,14 @@ def locker():
     '''
     return {"msg": "test"}
 
+
 @app.post("/{locker_id}/reserve/")
 def reserve(locker_id: int, locker: Locker):
     '''
     จอง
     '''
     return {"Item id": locker_id}
+
 
 @app.post("/{locker_id}/return_item")
 def return_item():
@@ -41,11 +63,13 @@ def return_item():
     '''
     pass
 
+
 @app.get("/{locker_id}/return_item/payment/")
 def payment():
     '''
     '''
     pass
-    
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
