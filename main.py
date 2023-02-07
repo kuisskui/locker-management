76f303686d3116e51095ea0ad76e7bc80d258ba0
Kaopong Sangkittiyakorn <kuisskui@Kaopongs-MacBook-Pro.local>
2023-02-07 19:26:31 +0700

    done without debug

--- main.py
+++ main.py
@@ -64,37 +64,32 @@ def reserve(locker_id: int, locker: Locker):
     return {"Item id": locker_id}
 
 
-@app.post("/{locker_id}/return_item/")
-def return_item(locker_id: str, itm: Item):
+@app.post("/lockerId/{locker_id}")
+def return_item(locker_id: str):
     '''
     return Item
     '''
-    fee = 0
-    late_fee = 0
 
-    info = collection.find({"user_id": str(itm.user_id)}, {"_id": False})
+    info = collection.find({"id": locker_id}, {"_id": False})
     var = list(info)[0]
-    receive_date = datetime.now()
     end_date = var['end']
     start_date = var['start']
+
     start_obj = datetime.strptime(start_date, FORMAT_DATETIME)
     end_obj = datetime.strptime(end_date, FORMAT_DATETIME)
-    duration = end_obj - start_obj
-
-    receive_duration = receive_date - start_obj
-
-    num_late = (receive_date - end_obj) / timedelta(minutes=60)
-    num_duration = duration / timedelta(minutes=60)
-    num_receive = receive_duration / timedelta(minutes=60)
-    if num_late > 0:
-        late_fee += (round(num_late*60)/10)*10
-    if num_duration > 2:
-        fee += (round(num_duration)-2)*5
-    if num_receive < 2:
-        change = 0
-
-    change = itm.amount - fee - late_fee
-    return {"Change": change, "amount": itm.amount}
+    reserve_time = (end_obj - start_obj).seconds
+    receive_time = (end_obj - datetime.now()).seconds
+    
+    charge = 0
+    if reserve_time/60/60 > 2:
+        charge = round(reserve_time/60/60 - 2)*5
+        
+    late = 0
+    if datetime.now() > end_obj:
+        late = round(receive_time/60/10)*20
+        
+    total = charge + late
+    return {"Change": total}
 
 
 if __name__ == "__main__":
